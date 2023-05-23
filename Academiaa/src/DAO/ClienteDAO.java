ba72600d7bfcdfb7aa0f652280097c3555421475
Marcos Matheus Medeiros N. Correa <181910174@senacinformatica.br>
2023-05-23 11:51:28 -0300

    Alteraçoes na DAOCliente

--- Academiaa/src/DAO/ClienteDAO.java
+++ Academiaa/src/DAO/ClienteDAO.java
@@ -1,15 +1,51 @@
 package DAO;
-import Conexao.Conexao;
-import Model.Cliente;
-import java.sql.Connection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import Conexao.Conexao;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import Model.Cliente;
 
 public class ClienteDAO {
-    
-    
-    public void cadClienteDAO(Cliente cVO){
-      Connection con = Conexao.getConexao();
-  }
 
+    public void cadClienteDAO(Cliente cVO) {
+        Connection con = Conexao.getConexao();
+        try {
+            String sql;
+            sql = "insert into cliente values (null,?,?,?,?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cVO.getNome());
+            pst.setString(2, cVO.getEndereco());
+            pst.setDate(3, cVO.getDataNascimento());
+            pst.setFloat(4, cVO.getAltura());
+            pst.setFloat(5, cVO.getPeso());
+            pst.setString(6, cVO.getUltimoAcesso());
+            pst.setString(7, cVO.getCpf());
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar Cliente!\n"
+                    + ex.getMessage());
+        }
+
+    }
+    public ArrayList<Cliente> getClientesDAO() {// Inicio GET
+        Connection con = Conexao.getConexao();
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select * from cliente";
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()){
+                Cliente c = new Cliente();
+                c.setIdCliente(rs.getInt("IDCliente"));
+                c.setNome(rs.getString("nome"));
+                
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
 }
-
--- Academiaa/src/Model/Cliente.java
+++ Academiaa/src/Model/Cliente.java
@@ -1,23 +1,29 @@
 package Model;
 
+import java.sql.Date;
+
 public class Cliente {
 
-
+    private int IdCliente;
     private int plano;
     private String nome;
     private String endereco;
+    private Date dataNascimento;
     private float altura;
     private float peso;
-    private float imc;
+    private String imc;
     private String ultimoAcesso;
     private String cpf;
 
     public Cliente() {
     }
-    public Cliente(int plano, String nome, String endereco, float altura, float peso, float imc, String ultimoAcesso, String cpf) {
+
+    public Cliente(int IdCliente, int plano, String nome, String endereco, Date dataNascimento, float altura, float peso, String imc, String ultimoAcesso, String cpf) {
+        this.IdCliente = IdCliente;
         this.plano = plano;
         this.nome = nome;
         this.endereco = endereco;
+        this.dataNascimento = dataNascimento;
         this.altura = altura;
         this.peso = peso;
         this.imc = imc;
@@ -25,64 +31,80 @@ public class Cliente {
         this.cpf = cpf;
     }
 
+    public int getIdCliente() {
+        return IdCliente;
+    }
+
+    public void setIdCliente(int IdCliente) {
+        this.IdCliente = IdCliente;
+    }
+
     public int getPlano() {
         return plano;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public float getAltura() {
-        return altura;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
-
-    public float getImc() {
-        return imc;
-    }
-
-    public String getUltimoAcesso() {
-        return ultimoAcesso;
-    }
-
-    public String getCpf() {
-        return cpf;
     }
 
     public void setPlano(int plano) {
         this.plano = plano;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
     public void setAltura(float altura) {
         this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
     }
 
     public void setPeso(float peso) {
         this.peso = peso;
     }
 
-    public void setImc(float imc) {
+    public String getImc() {
+        return imc;
+    }
+
+    public void setImc(String imc) {
         this.imc = imc;
+    }
+
+    public String getUltimoAcesso() {
+        return ultimoAcesso;
     }
 
     public void setUltimoAcesso(String ultimoAcesso) {
         this.ultimoAcesso = ultimoAcesso;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 
     public void setCpf(String cpf) {
@@ -91,7 +113,9 @@ public class Cliente {
 
     @Override
     public String toString() {
-        return "Cliente{" + "plano=" + plano + ", nome=" + nome + ", endereco=" + endereco + ", altura=" + altura + ", peso=" + peso + ", imc=" + imc + ", ultimoAcesso=" + ultimoAcesso + ", cpf=" + cpf + '}';
+        return "Cliente{" + "IdCliente=" + IdCliente + ", plano=" + plano + ", nome=" + nome + ", endereco=" + endereco + ", dataNascimento=" + dataNascimento + ", altura=" + altura + ", peso=" + peso + ", imc=" + imc + ", ultimoAcesso=" + ultimoAcesso + ", cpf=" + cpf + '}';
     }
 
+    
+    
 }
